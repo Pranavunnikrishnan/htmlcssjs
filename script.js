4b60a5de4d70fe63fb30c1ea30948feb7224b139
shihadshadhu <shadhunihal55@gmail.com>
2023-03-16 14:57:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let fruits=["apple","orange","banana","grapes"]
+for(let i=0;i<fruits.length;i++)
+{
+    console.log(fruits[i])
+}
+let p=0;
+while(p<fruits.lenght)
+{
+    console.log(fruits[p])
+    p++;
+}
+let q=0;
+do{
+    console.log(fruits[q])
+    q++;
+}while(q<fruits.length);
+
+function add(a,b){
+    let sum=a+b;
+    console.log(sum)
+}
+add(2,4);
+
+const addd = (a,b)=>{
+    console.log(a+b);
+}
+addd(2,4);
+
+const mult = (a,b)=>{
+    console.log(a*b);
+}
+mult(2,2);
+
